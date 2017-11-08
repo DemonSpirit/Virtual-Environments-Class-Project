6b426a2b990c5a4e0712d67c4a8147bd13ab2c51
Ben Tandy <ben@afriendcalledben.com>
2017-11-08 17:30:11 +0000

    Upload of Object Change Test

---
+++ Virtual Environments Class Project/Assets/HideScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideScript : MonoBehaviour {
+
+    public Camera playerHead;
+    public GameObject plant;
+    private bool plantVisible = true;
+    private bool changeEnabled = true;
+    private int outOfSightCount = 0;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (!I_Can_See(plant))
+        {
+            if (changeEnabled)
+            {
+                outOfSightCount++;
+                if (outOfSightCount > 50)
+                {
+                    if (!plantVisible)
+                    {
+                        plant.SetActive(true);
+                        plantVisible = true;
+                    }
+                    else
+                    {
+                        plant.SetActive(false);
+                        plantVisible = false;
+                    }
+                    outOfSightCount = 0;
+                    changeEnabled = false;
+                }
+            }
+        } else
+        {
+            outOfSightCount = 0;
+            changeEnabled = true;
+        }
+    }
+
+    private bool I_Can_See(GameObject Object)
+    {
+        Plane[] planes = GeometryUtility.CalculateFrustumPlanes(playerHead);
+        if (GeometryUtility.TestPlanesAABB(planes, Object.GetComponent<Collider>().bounds))
+            return true;
+        else
+            return false;
+    }
+}
