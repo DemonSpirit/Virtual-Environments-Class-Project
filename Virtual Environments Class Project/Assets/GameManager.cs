7130420c904c46975684db03b4a6331d298ef0ed
tommygraven <tommygraven@me.com>
2017-11-18 14:26:38 +0000

    Switch Controller by checkbox
    Choose between VR and non VR character controllers by checkbox on the
    gameManager gameObject

---
+++ Virtual Environments Class Project/Assets/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	[SerializeField] bool isVR;
+	[SerializeField] GameObject VRCharacter;
+	[SerializeField] GameObject FPSCharacter;
+
+
+	void Awake()
+	{
+		VRCharacter.SetActive(isVR);
+		FPSCharacter.SetActive(!isVR);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
