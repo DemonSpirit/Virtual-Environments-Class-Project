88fe2bbc23fc5de5e19d72d0eb8caefc2fd9e105
Ben Tandy <ben@afriendcalledben.com>
2017-11-27 10:54:39 +0000

    Add glow plugin

---
+++ Virtual Environments Class Project/Assets/SteamVR/TriggerTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerTest : MonoBehaviour {
+
+    public GameObject hand;
+    private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+    private SteamVR_trackedObject trackedObject;
+    private SteamVR_TrackedController.Device device;
+
+	// Use this for initialization
+	void Start () {
+        trackedObject = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
